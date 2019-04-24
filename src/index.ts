dff37eab4546d7bbc13db51a5ac32921ee6f6b40
Jim Raynor <paolo@combi.li>
2019-04-24 17:54:29 +0200

    Move (unsuccessfully due to the not-published on NPM) the script to Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-import * as Qlik from 'qlik';
 
+import { Extension } from "qlik";
+
+
+define( [ "qlik"
+],
+function (qlik):Extension {
+
+	return {
+		support : {
+			snapshot: true,
+			export: true,
+			exportData : false
+		},
+		paint: function ($element:JQuery) {
+			//add your rendering code here
+			$element.html( "basic" );
+			//needed for export
+			return qlik.Promise.resolve();
+		}
+	};
+
+} );
